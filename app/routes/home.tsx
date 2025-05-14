641e4b012969c174a7bd81970422742bb9a93e48
Takahiro Tanaka <takahiro.tanaka.job@gmail.com>
2025-05-14 12:02:14 +0900

    tailwindcss @tailwindcss/vite 削除

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,5 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
-
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
-  return <Welcome />;
+  return <div>
+    <h1>Welcome to Remix</h1>
+  </div>;
 }
